4898c684e386a7e473a0b353e7079f6c8819adf1
brown5lc <brown5lc@mail.uc.edu>
2026-03-17 19:45:22 -0400

    Added default nav bar and placeholder logo

--- authored_frontend/src/main.tsx
+++ authored_frontend/src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Dashboard from './pages/Homepage.tsx'
 // Render router here
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Dashboard />
   </StrictMode>,
 )
---
+++ authored_frontend/src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import ResponsiveAppBar from '../components/ResponsiveAppBar'
+
+function Dashboard(){
+    return(
+        <div>
+        <ResponsiveAppBar />
+        <h1>Ass</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
